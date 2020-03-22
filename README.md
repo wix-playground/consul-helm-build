dfa648be430e2bfb3384a0b8e15e3ed7f53d9f19
alexshe <alexshe@wix.com>
2020-03-22 15:06:21 +0200

    Build script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Consul helm chart build
+Official [consul helm chart](https://github.com/hashicorp/consul-helm) is not published.
+This repo includes custom travis build file that will publish hashicorp consul helm chart to http://helm-repo.sphera.tools.s3-website-us-east-1.amazonaws.com/consul
---
+++ build-chart.sh
@@ -0,0 +1,16 @@
+mkdir build
+mkdir prevdata
+
+git clone git@github.com:hashicorp/consul-helm.git consul
+aws s3 cp s3://helm-repo.sphera.tools/consul prevdata --recursive || true
+cd consul && helm package ./ && cd ..
+mv consul/*.tgz build/
+if test -f prevdata/index.yaml; then
+    echo 'Merging with previous data'
+    mv prevdata/*.tgz build/ || true
+    helm repo index build/ --merge prevdata/index.yaml --url http://helm-repo.sphera.tools.s3-website-us-east-1.amazonaws.com/consulv3
+else
+    echo 'No previous charts to merge'
+    helm repo index build/ --url http://helm-repo.sphera.tools.s3-website-us-east-1.amazonaws.com/consulv3
+fi
+
